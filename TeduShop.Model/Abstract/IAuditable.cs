1e422cc95ee0fdfab58c97a0f0a5abfdc51e2df9
LuongLV\Le Van Luong <luongle12@gmail.com>
2017-11-30 22:06:54 +0700

    Hoan thanh Model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime CreateDate { set; get; }
+        string CreateBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        [MaxLength(50)]
+        public string Type { set; get; }
+    }
+}
